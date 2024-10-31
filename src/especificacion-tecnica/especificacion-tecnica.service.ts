bc2337857f63ecb8fd0a990e1aed226e713b3ff6
EliSLopezM <emprendimientoeli@gmail.com>
2024-10-31 03:50:07 -0500

    feat: Creacion de servicio para el modulo especificacion-tecnica udistrital/argo_documentacion#186

--- src/especificacion-tecnica/especificacion-tecnica.service.ts
+++ src/especificacion-tecnica/especificacion-tecnica.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CrearEspecificacionTecnicaDto } from './dto/crear-especificacion-tecnica.dto';
+import { EspecificacionTecnica } from './entities/especificacion-tecnica.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ActualizarEspecificacionTecnicaDto } from './dto/actualizar-especificacion-tecnica';
 
 @Injectable()
-export class EspecificacionTecnicaService {}
+export class EspecificacionTecnicaService {
+  constructor(
+    @InjectRepository(EspecificacionTecnica)
+    private especificacionTecnicaRepository: Repository<EspecificacionTecnica>,
+  ) {}
+
+  async findAll(): Promise<EspecificacionTecnica[]> {
+    return this.especificacionTecnicaRepository.find();
+  }
+
+  async findOne(id: number): Promise<EspecificacionTecnica> {
+    const especificacion = await this.especificacionTecnicaRepository.findOne({
+      where: { id },
+    });
+    if (!especificacion) {
+      throw new NotFoundException(`EspecificacionTecnica con ID "${id}" no encontrada`);
+    }
+    return especificacion;
+  }
+
+  async create(
+    especificacionTecnicaDto: CrearEspecificacionTecnicaDto,
+  ): Promise<EspecificacionTecnica> {
+    const especificacion = this.especificacionTecnicaRepository.create(especificacionTecnicaDto);
+    return this.especificacionTecnicaRepository.save(especificacion);
+  }
+
+  async update(
+    id: number,
+    especificacionTecnicaDto: ActualizarEspecificacionTecnicaDto,
+  ): Promise<EspecificacionTecnica> {
+    await this.especificacionTecnicaRepository.update(id, especificacionTecnicaDto);
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    const especificacion = await this.findOne(id);
+    if (!especificacion) {
+      throw new NotFoundException(`EspecificacionTecnica con ID "${id}" no encontrada`);
+    }
+    await this.especificacionTecnicaRepository.update(id, {
+      activo: false,
+      fechaModificacion: new Date(),
+    });
+  }
+}
