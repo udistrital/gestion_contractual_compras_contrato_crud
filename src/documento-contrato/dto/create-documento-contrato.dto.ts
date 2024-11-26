870461b68244d4dc324689f41c6d36789e3b746e
Mauricio Alvarado <mauriciorianoa@gmail.com>
2024-11-25 20:04:25 -0500

    feat: Actualización Swagger

--- src/documento-contrato/dto/create-documento-contrato.dto.ts
+++ src/documento-contrato/dto/create-documento-contrato.dto.ts
@@ -1,6 +1,11 @@
 import { IsNumber, IsBoolean, IsString, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateDocumentoContratoDto {
+  @ApiProperty({
+    example: 1,
+    description: 'Documento ID - Gestor Documental',
+  })
   @IsNumber()
   documento_id: number;
 
@@ -8,13 +13,26 @@ export class CreateDocumentoContratoDto {
   @IsOptional()
   activo?: boolean;
 
+  @ApiProperty({
+    example: 1,
+    description: 'ID Contrato General Asociado',
+  })
   @IsNumber()
   contrato_general_id: number;
 
+  @ApiProperty({
+    example: '123e4567-e89b-12d3-a456-426614174000',
+    description: 'Enlace al documento',
+  })
   @IsString()
   @IsOptional()
   documento_enlace: string;
 
+  @ApiProperty({
+    example: 1,
+    required: false,
+    description: 'ID Tipo Documento - Parámetros CRUD',
+  })
   @IsNumber()
   @IsOptional()
   tipo_documento_id: number;
--- src/documento-contrato/entities/documento-contrato.entity.ts
+++ src/documento-contrato/entities/documento-contrato.entity.ts
@@ -6,15 +6,30 @@ import {
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { ContratoGeneral } from '../../contrato-general/entities/contrato-general.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class DocumentoContrato {
+  @ApiProperty({
+    example: '123e4567-e89b-12d3-a456-426614174000',
+    description: 'Identificador único del documento',
+  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty({
+    example: 1,
+    required: false,
+    description: 'ID Tipo Documento - Parámetros CRUD',
+  })
   @Column({ type: 'integer', nullable: true })
   tipo_documento_id: number;
 
+  @ApiProperty({
+    example: '123e4567-e89b-12d3-a456-426614174000',
+    required: false,
+    description: 'Enlace al documento',
+  })
   @Column({ type: 'varchar', length: 50, nullable: true })
   documento_enlace: string;
 
