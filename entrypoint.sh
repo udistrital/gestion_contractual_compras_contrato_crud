581b6d0b108158043eca9b811a6f633a2dcbe50d
Heiner Edwar Granados Leal <50187485+edwargl7@users.noreply.github.com>
2025-01-28 21:52:49 -0500

    fix: Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,7 @@ set -o pipefail
 export GESTION_CONTRACTUAL_CRUD_USERNAME="$(aws ssm get-parameter --name /${PARAMETER_STORE}/gestion_contractual_crud/db/username --output text --query Parameter.Value)"
 export GESTION_CONTRACTUAL_CRUD_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/gestion_contractual_crud/db/password --output text --query Parameter.Value)"
 
+echo "$GESTION_CONTRACTUAL_CRUD_USERNAME"
+echo "$GESTION_CONTRACTUAL_CRUD_PASS"
+
 exec node dist/main
